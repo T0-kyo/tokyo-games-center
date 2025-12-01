d2712702a91875a27302edd44a29c33dbdd4d368
T0-kyo <alhussain.akah.123@gmail.com>
2025-12-01 11:42:17 +0200

    added 5x5_TTT

---
+++ 5x5_TTT/5x5_TTT_Board.cpp
@@ -0,0 +1,92 @@
+#include "5x5_TTT_Board.h"
+
+TTT5x5_Board::TTT5x5_Board() : Board<char>(5,5) {
+    p1_score = 0;
+    p2_score = 0;
+
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<columns; j++){
+            board[i][j] = ' ';
+        }
+    }
+}
+
+bool TTT5x5_Board::update_board(Move<char>* move) {
+    int x = move->get_x();
+    int y = move->get_y();
+
+    if (x<0 || x>4 || y<0 || y>4)
+        return false;
+
+    if(board[x][y] !=' ')
+        return false;
+
+    board[x][y] = move->get_symbol();
+    n_moves++;
+    return true;
+}
+
+void TTT5x5_Board::add_point(Player<char>* player){
+    char sym = player->get_symbol();
+    int points = 0;
+
+    // Check rows for 3 in a row
+    for (int r=0; r<5; r++){
+        for (int c=0; c<3; c++){
+            if (board[r][c] == sym && board[r][c+1] == sym && board[r][c+2] == sym){
+                    points++;
+            }
+        }
+    }
+
+    // Check columns for 3 in a row
+    for (int c=0; c<5; c++){
+        for (int r=0; r<3; r++){
+            if (board[r][c] == sym && board[r+1][c] == sym && board[r+2][c] == sym){
+                points++;
+            }
+        }
+    }
+
+    // Check diagonals for 3 in a row
+    for (int r=0; r<3; r++){
+        for (int c=0; c<3; c++){
+            if (board[r][c] == sym && board[r+1][c+1] == sym && board[r+2][c+2] == sym){
+                points++;
+            }
+        }
+        for (int c=4; c>1; c--){
+            if(board[r][c] == sym && board[r+1][c-1] == sym && board[r+2][c-2] == sym){
+                points++;
+            }
+        }
+    }
+    
+
+    if(player->get_symbol() == 'X'){
+        p1_score = points;
+    } else {
+        p2_score = points;
+    }
+}
+
+bool TTT5x5_Board::game_is_over(Player<char>* player) {
+    
+    return (n_moves == 25);
+}
+
+bool TTT5x5_Board::is_win(Player<char>* player) {
+
+    add_point(player);
+    return (game_is_over(player) && p1_score > p2_score);
+}
+
+bool TTT5x5_Board::is_draw(Player<char>* player) {
+
+    return (game_is_over(player) && p1_score == p2_score); 
+}
+
+bool TTT5x5_Board::is_lose(Player<char>* player) {
+
+    return (game_is_over(player) && p1_score < p2_score);
+}
---
+++ 5x5_TTT/5x5_TTT_Board.h
@@ -0,0 +1,31 @@
+#ifndef TTT5x5_BOARD_H
+#define TTT5x5_BOARD_H
+
+#include "../BoardGameFramework/BoardGame_Classes.h"
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class TTT5x5_Board : public Board<char> {
+    private:
+    int p1_score;
+    int p2_score;
+    void add_point(Player<char>* player); // scoring function
+
+    public:
+    TTT5x5_Board();
+
+    bool update_board(Move<char>* move) override;
+    bool is_win(Player<char>* player) override;
+    bool is_draw(Player<char>* player) override;
+    bool game_is_over(Player<char>* player) override;
+    bool is_lose(Player<char>* player) override;
+
+    // دوال إضافية لعرض النقاط في الـ UI
+    int get_p1_score() { return p1_score; }
+    int get_p2_score() { return p2_score; }
+};
+
+#endif // TTT5x5_BOARD_H
