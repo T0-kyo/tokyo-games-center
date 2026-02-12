fc52aa790b3b2a2c3c482e02ee0d47696e6d0ee0
T0-kyo <alhussain.akah.123@gmail.com>
2026-02-12 16:27:38 +0200

    Renamed the SFML folder to avoid conflicts with the SFML library and added GamaManager (Game loop) and updated CmakeLists

--- GameManager/State.h
+++ GameManager/State.h
@@ -9,7 +9,7 @@ namespace Tokyo {
         virtual void Init() = 0;    //Initialize the state
 
         virtual void HandleInput() = 0;    //Handle user input
-        virtual void Update() = 0;    //Updates any variables or objects after receiving input
+        virtual void Update( float dt ) = 0;    //Updates any variables or objects after receiving input
         virtual void Draw( float dt ) = 0;   //Draws the state to the screen with a delta time for smooth animations
 
         virtual void Pause() {};    //Pauses the current state, typically when another state is added on top of it (e.g. when the pause menu is opened during gameplay)
