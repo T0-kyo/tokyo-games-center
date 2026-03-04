8fe1b8e5b1b5fd97cffa8ff935baaf1f586fadb6
T0-kyo <alhussain.akah.123@gmail.com>
2026-03-04 22:14:41 +0200

    added 5x5 game state and it's sprites

--- GameCenter/5x5_TTT/5x5State.h
+++ GameCenter/5x5_TTT/5x5State.h
@@ -3,9 +3,13 @@
 #include "../../DEFENITIONS.h"
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
+#include "5x5_TTT_Board.h"
+#include <cstdlib>
 #include GAME_PLAY
 #include GAME_PATH
 #include STATE_PATH
+#include GAMEOVER_STATE
+#include PAUSE_STATE
 
 
 namespace Tokyo {
@@ -21,5 +25,39 @@ namespace Tokyo {
 
         private:
         GameDataRef _data;
+
+        std::shared_ptr<Player<char>> _Player1;
+        std::shared_ptr<Player<char>> _Player2;
+        std::shared_ptr<TTT5x5_Board> _5x5Board;
+        sf::Clock _clock;
+        sf::Clock _gameOverClock;
+
+        std::unique_ptr<sf::Sprite> _background;
+        std::unique_ptr<sf::Sprite> _pauseButton;
+        std::unique_ptr<sf::Sprite> _grid;
+        std::unique_ptr<sf::Sprite> _x;
+        std::unique_ptr<sf::Sprite> _o;
+        std::unique_ptr<sf::Sprite> _xWin;
+        std::unique_ptr<sf::Sprite> _oWin;
+
+        std::unique_ptr<sf::Text> _player1;
+        std::unique_ptr<sf::Text> _player2;
+        std::unique_ptr<sf::Text> _player1Turn;
+        std::unique_ptr<sf::Text> _player2Turn;
+        std::unique_ptr<sf::Text> _score1;
+        std::unique_ptr<sf::Text> _score2;
+
+        bool _p1 = false;
+        bool _p2 = false;
+        bool _draw = false;
+
+        Player<char>* _currentPlayer;
+        PlayerType _playerType;
+
+        int _row;
+        int _col;
+        float CellWidth=0;
+        float CellHeight=0;
+        sf::Vector2f gridPos;
     };
 }
