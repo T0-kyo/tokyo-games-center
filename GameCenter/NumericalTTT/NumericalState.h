ae53d6583dae20a18eda8fb10ad5480a84d58e5d
T0-kyo <alhussain.akah.123@gmail.com>
2026-03-22 03:02:56 +0200

    added Numerical tic tac toe state

--- GameCenter/NumericalTTT/NumericalState.h
+++ GameCenter/NumericalTTT/NumericalState.h
@@ -3,9 +3,13 @@
 #include "../../DEFENITIONS.h"
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
+#include "NumericalTTT_Board.h"
+#include <cstdlib>
 #include GAME_PLAY
 #include GAME_PATH
 #include STATE_PATH
+#include GAMEOVER_STATE
+#include PAUSE_STATE
 
 
 namespace Tokyo {
@@ -21,5 +25,42 @@ namespace Tokyo {
 
         private:
         GameDataRef _data;
+
+        std::shared_ptr<Player<int>> _Player1;
+        std::shared_ptr<Player<int>> _Player2;
+        std::shared_ptr<NumericalTTT_Board> _NumericalBoard;
+        sf::Clock _clock;
+        sf::Clock _gameOverClock;
+
+        std::unique_ptr<sf::Sprite> _background;
+        std::unique_ptr<sf::Sprite> _pauseButton;
+        std::unique_ptr<sf::Sprite> _grid;
+        std::unique_ptr<sf::Sprite> _num;
+        std::unique_ptr<sf::Sprite> _currentCell;
+
+        std::unique_ptr<sf::Text> _player1;
+        std::unique_ptr<sf::Text> _player2;
+        std::unique_ptr<sf::Text> _player1Turn;
+        std::unique_ptr<sf::Text> _player2Turn;
+        std::unique_ptr<sf::Text> _player1Nums;
+        std::unique_ptr<sf::Text> _player2Nums;
+
+        std::vector<int> _p1_nums;
+        std::vector<int> _p2_nums;
+        std::map<int, bool> _isUsed;
+
+        bool _p1 = false;
+        bool _p2 = false;
+        bool _draw = false;
+
+        Player<int>* _currentPlayer;
+        PlayerType _playerType;
+        bool _cellChosen = false;
+        
+        int _row;
+        int _col;
+        float CellWidth=0;
+        float CellHeight=0;
+        sf::Vector2f gridPos;
     };
 }
